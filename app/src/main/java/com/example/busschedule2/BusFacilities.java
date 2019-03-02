9a754f15a1f45bbe9f7b5c310fb34d55c43e2e7c
nayananga@acerubuntu18.04 <nayanangamuhandiram@gmail.com>
2019-03-02 19:01:34 +0530

    Code Optimezed

--- app/src/main/java/com/example/busschedule2/BusFacilities.java
+++ app/src/main/java/com/example/busschedule2/BusFacilities.java
@@ -1,11 +1,15 @@
 package com.example.busschedule2;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 
 public class BusFacilities extends AppCompatActivity {
 
@@ -14,12 +18,24 @@ public class BusFacilities extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_facilities);
 
+        ListView listView = findViewById(R.id.facilityList);
+        ArrayList<String> facilityList = new ArrayList<>();
+        facilityList.add("Non A/C");
+        facilityList.add("Wifi");
+        facilityList.add("TV");
+        facilityList.add("Comfortable Seats");
+        facilityList.add("Phone Charging");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_facility_item, R.id.text4, facilityList);
+        listView.setAdapter(adapter);
+
+
         configureBackButton2();
         configureConfirmButton();
 
     }
-    private void configureBackButton2(){
-        Button backButton2 = (Button) findViewById(R.id.backButton2);
+
+    private void configureBackButton2() {
+        Button backButton2 = findViewById(R.id.backButton2);
         backButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,13 +44,13 @@ public class BusFacilities extends AppCompatActivity {
         });
     }
 
-    private void  configureConfirmButton(){
-        Button confirmButton = (Button) findViewById(R.id.confirmButton);
+    private void configureConfirmButton() {
+        Button confirmButton = findViewById(R.id.confirmButton);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast  toast = Toast.makeText(getApplicationContext(), "Thank You", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "Thank You", Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
--- app/src/main/java/com/example/busschedule2/BusList.java
+++ app/src/main/java/com/example/busschedule2/BusList.java
@@ -23,23 +23,22 @@ public class BusList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_list);
 
-        ListView resultsListView = (ListView) findViewById(R.id.bus_list);
+        ListView resultsListView = findViewById(R.id.bus_list);
         HashMap<String,String> nameAddresses = new HashMap<>();
-        nameAddresses.put("Nipuna","Mathale");
-        nameAddresses.put("Samila","Horana");
-        nameAddresses.put("Anuka","Gampaha");
-        nameAddresses.put("Shehan","Anuradhapura");
-        nameAddresses.put("Nayananga","Ratnapura");
+        nameAddresses.put("8 p.m","Ampara to Colombo");
+        nameAddresses.put("8.30 p.m","Akkarepatthu to Colombo");
+        nameAddresses.put("9 p.m","Kalmunei to Colombo");
+//        nameAddresses.put("Shehan","Anuradhapura");
+//        nameAddresses.put("Nayananga","Ratnapura");
 
         List<HashMap<String,String>> listItems = new ArrayList<>();
         SimpleAdapter adapter = new SimpleAdapter(this,listItems,R.layout.list_item, new String[]{"First Line","Second Line"}, new int[]{R.id.text1,R.id.text3});
 
-        Iterator it = nameAddresses.entrySet().iterator();
-        while (it.hasNext()){
-            HashMap<String,String> resultsMap = new HashMap<>();
-            Map.Entry pair = (Map.Entry)it.next();
-            resultsMap.put("First Line",pair.getKey().toString());
-            resultsMap.put("Second Line",pair.getValue().toString());
+        for (Object o : nameAddresses.entrySet()) {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry) o;
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
             listItems.add(resultsMap);
         }
 
@@ -57,7 +56,7 @@ public class BusList extends AppCompatActivity {
     }
 
     private void configureBackButton(){
-        Button backButton = (Button) findViewById(R.id.backButton);
+        Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
